cc4577cb46d4d0c5778f2af125bbda1259b8df4d
dobestan <dobestan@gmail.com>
2014-02-17 21:33:05 +0900

    complete DaumAlbum

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Humhum::Application.routes.draw do
   resources :albums
   resources :naver_albums
+  resources :daum_albums
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217111201) do
+ActiveRecord::Schema.define(version: 20140217121937) do
 
   create_table "albums", force: true do |t|
     t.string   "content_provider"
-    t.string   "title"
-    t.string   "artist"
-    t.string   "youtube_hash"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "naver_albums", force: true do |t|
-    t.string   "content_provider", default: "N"
     t.string   "title"
     t.string   "artist"
     t.string   "youtube_hash"
